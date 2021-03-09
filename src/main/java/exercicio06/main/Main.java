6453727fc7975078ffbdbfeb6ad224bf6f30d779
Vinicius <viniciuslobato007@gmail.com>
2021-03-09 20:30:58 -0300

    Finalização do problema, porém com erros...

--- src/main/java/exercicio06/main/Main.java
+++ src/main/java/exercicio06/main/Main.java
@@ -4,6 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Main app = new Main();
+
+		app.start();
+	}
+
+	private void start() {
+
+		String[] cars = { "civic", "gol", "palio", "uno" };
+
+		int max = cars.length;
+
+		int i = 0;
+
+		while (i <= max) {
+			System.out.println("Modelo: " + cars[i]);
+			i++;
+		}
+
 	}
 
 }
